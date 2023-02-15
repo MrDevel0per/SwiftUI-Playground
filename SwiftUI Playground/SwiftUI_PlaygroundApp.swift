17cfe772e3dcc20c38678ab4e44262abf441bc4c
MrDevel0per <mrdeveloper081@gmail.com>
2023-02-15 10:23:17 -0700

    Initial Commit

---
+++ SwiftUI Playground/SwiftUI_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PlaygroundApp.swift
+//  SwiftUI Playground
+//
+//  Created by Owen Cruz-Abrams on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
